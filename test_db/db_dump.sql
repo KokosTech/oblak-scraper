a0a3df6e5a5c73cabddc6471703bdda6849ff4f3
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-18 00:13:47 +0300

    [MOVE] db to test db

--- test_db/db_dump.sql
+++ test_db/db_dump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 127.0.0.1    Database: oblak_scraper
 -- ------------------------------------------------------
--- Server version	10.11.2-MariaDB
+-- Server version	10.11.2-MariaDB-1:10.11.2+maria~ubu2204
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +30,7 @@ CREATE TABLE `images` (
   PRIMARY KEY (`id`),
   KEY `website_id` (`website_id`),
   CONSTRAINT `images_ibfk_1` FOREIGN KEY (`website_id`) REFERENCES `websites` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,9 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES
+(1,'url_to_img','alt kris',1),
+(2,'https://res.cloudinary.com/dxzlwo0nt/image/upload/v1681647328/websites/website_4_TUES%20Fest%202023_Alt.jpg','about',3);
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +57,7 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `passhash` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +66,11 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES
+(1,'kaloyan','$2b$10$g8ptPX0ITAzMPKPUtKy2mu0ACNLmwZX2x/43WIs1elP6AeXHC/Xda'),
+(2,'kaloyan2','$2b$10$zH//Vxa5myInVhW5OS6ag.SKKDtpUBpX9Bra0TqujsVCw6jjUKLdW'),
+(3,'kaloyan3','$2b$10$uFEUWb3DVN9hJp6CSKxWnurQykxMQOHaAB3fgdND0Pfgycc6wIiny'),
+(4,'kaloyan4','$2b$10$A0O5siKlN9/suL7yYkan6O5LOJzqcpDrqr/PByBGMMuduyjCfgwzG');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +90,7 @@ CREATE TABLE `websites` (
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `websites_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +99,10 @@ CREATE TABLE `websites` (
 
 LOCK TABLES `websites` WRITE;
 /*!40000 ALTER TABLE `websites` DISABLE KEYS */;
+INSERT INTO `websites` VALUES
+(1,'TUES Fest 2023','https://tuesfest.bg','Textly text',4),
+(2,'Google Search','https://google.com','More text',4),
+(3,'TUES Fest 2023','https://tuesfest.bg/','kur',4);
 /*!40000 ALTER TABLE `websites` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -103,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-12 23:26:55
+-- Dump completed on 2023-04-16 15:36:42
